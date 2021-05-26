7db5c6059875fac9ade8fff41e849e6d9906eb03
CarlosErencia <a19carerecas@inspedralbes.cat>
2021-05-26 17:41:50 +0200

    update build

---
+++ build/precache-manifest.a2405844019ef21be9fbed5431030d11.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9be21e747660bf2c82287af72375a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a251ef0aa58c34aeaaca",
+    "url": "/static/css/2.97a74c01.chunk.css"
+  },
+  {
+    "revision": "a142509b76b4be4950fa",
+    "url": "/static/css/main.b9b797b9.chunk.css"
+  },
+  {
+    "revision": "a251ef0aa58c34aeaaca",
+    "url": "/static/js/2.0ed3b47e.chunk.js"
+  },
+  {
+    "revision": "ec639718c209d83c9234d6b3862c5881",
+    "url": "/static/js/2.0ed3b47e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a142509b76b4be4950fa",
+    "url": "/static/js/main.40f6b92d.chunk.js"
+  },
+  {
+    "revision": "9d96126a8f18c0ebbaa0",
+    "url": "/static/js/runtime-main.20a4d22a.js"
+  },
+  {
+    "revision": "cab96d33d8bb6c356d33161327c8401f",
+    "url": "/static/media/Habitaciones.cab96d33.jpg"
+  },
+  {
+    "revision": "af74fd749e081bd534e720ef78d3ae11",
+    "url": "/static/media/Home.af74fd74.jpeg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "6edb36f3feccea6f8483d2808828b6e1",
+    "url": "/static/media/error.6edb36f3.jpg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/static/media/room-9.7b17c49f.jpeg"
+  }
+]);
